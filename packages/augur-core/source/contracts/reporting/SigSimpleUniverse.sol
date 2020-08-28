dc6b1bfe69259b850b0dda1b3147e969cf2994b2
zandent <simon.zhao@mail.utoronto.ca>
2020-08-28 04:04:41 -0400

    augur with sig passed

--- packages/augur-core/source/contracts/reporting/SigSimpleUniverse.sol
+++ packages/augur-core/source/contracts/reporting/SigSimpleUniverse.sol
@@ -3,11 +3,16 @@ pragma solidity >=0.5.10;
 
 import 'ROOT/reporting/ISimpleUniverse.sol';
 import 'ROOT/libraries/math/SafeMathUint256.sol';
-import 'ROOT/Cash.sol';
-import 'ROOT/TestNetDaiVat.sol';
-import 'ROOT/TestNetDaiPot.sol';
-import 'ROOT/TestNetDaiJoin.sol';
+// import 'ROOT/Cash.sol';
+// import 'ROOT/TestNetDaiVat.sol';
+// import 'ROOT/TestNetDaiPot.sol';
+// import 'ROOT/TestNetDaiJoin.sol';
 import 'ROOT/Augur.sol';
+import 'ROOT/external/IDaiVat.sol';
+import 'ROOT/external/IDaiPot.sol';
+import 'ROOT/external/IDaiJoin.sol';
+//import 'ROOT/IAugur.sol';
+import 'ROOT/Cash.sol';
 
 
 /**
@@ -16,7 +21,6 @@ import 'ROOT/Augur.sol';
  */
 contract SigSimpleUniverse is ISimpleUniverse {
     using SafeMathUint256 for uint256;
-    
     // a constant that should be set to the daily block rate of the network
     uint public DAILYBLOCKRATE;
 
@@ -25,9 +29,9 @@ contract SigSimpleUniverse is ISimpleUniverse {
     // DAI / DSR specific
     uint256 public totalBalance;
     Cash public cash;
-    TestNetDaiVat public daiVat;
-    TestNetDaiPot public daiPot;
-    TestNetDaiJoin public daiJoin;
+    IDaiVat public daiVat;
+    IDaiPot public daiPot;
+    IDaiJoin public daiJoin;
 
     uint256 constant public DAI_ONE = 10 ** 27;
 
@@ -41,16 +45,16 @@ contract SigSimpleUniverse is ISimpleUniverse {
         emitsig DailySignal().delay(DBR);
     }
 
-    function saveDaiInDSR(uint256 _amount) private returns (bool) {
-        daiJoin.join(address(this), _amount);
-        daiPot.drip();
+    function saveDaiInDSR(uint256 _amount) public returns (bool) {
+        //daiJoin.join(address(this), _amount);
+        //daiPot.drip();
         uint256 _sDaiAmount = _amount.mul(DAI_ONE) / daiPot.chi(); // sDai may be lower than the full amount joined above. This means the VAT may have some dust and we'll be saving less than intended by a dust amount
-        daiPot.join(_sDaiAmount);
+        //daiPot.join(_sDaiAmount);
         return true;
     }
 
-    function withdrawDaiFromDSR(uint256 _amount) private returns (bool) {
-        daiPot.drip();
+    function withdrawDaiFromDSR(uint256 _amount) public returns (bool) {
+        //daiPot.drip();
         uint256 _chi = daiPot.chi();
         uint256 _sDaiAmount = _amount.mul(DAI_ONE) / _chi; // sDai may be lower than the amount needed to retrieve `amount` from the VAT. We cover for this rounding error below
         if (_sDaiAmount.mul(_chi) < _amount.mul(DAI_ONE)) {
@@ -61,15 +65,16 @@ contract SigSimpleUniverse is ISimpleUniverse {
         return true;
     }
 
-    function withdrawSDaiFromDSR(uint256 _sDaiAmount) private returns (bool) {
-        daiPot.exit(_sDaiAmount);
-        daiJoin.exit(address(this), daiVat.dai(address(this)).div(DAI_ONE));
+    function withdrawSDaiFromDSR(uint256 _sDaiAmount) public returns (bool) {
+        //daiPot.exit(_sDaiAmount);
+        //daiJoin.exit(address(this), daiVat.dai(address(this)).div(DAI_ONE));
         return true;
     }
 
     function deposit(address _sender, uint256 _amount, address _market) public returns (bool) {
         require(augur.isTrustedSender(msg.sender) || msg.sender == _sender);
-        augur.trustedTransfer(cash, _sender, address(this), _amount);
+        bool success = augur.trustedTransfer(cash, _sender, address(this), _amount);
+        require(success, "Transfer failed");
         totalBalance = totalBalance.add(_amount);
         marketBalance[_market] = marketBalance[_market].add(_amount);
         saveDaiInDSR(_amount);
@@ -90,13 +95,13 @@ contract SigSimpleUniverse is ISimpleUniverse {
 
     function sweepInterest() public returns (bool) {
         uint256 _extraCash = 0;
-        daiPot.drip();
+        //daiPot.drip();
         withdrawSDaiFromDSR(daiPot.pie(address(this))); // Pull out all funds
         saveDaiInDSR(totalBalance); // Put the required funds back in savings
         _extraCash = cash.balanceOf(address(this));
         // The amount in the DSR pot and VAT must cover our totalBalance of Dai
-        assert(daiPot.pie(address(this)).mul(daiPot.chi()).add(daiVat.dai(address(this))) >= totalBalance.mul(DAI_ONE));
-        cash.transfer(address(this), _extraCash);
+        require(daiPot.pie(address(this)).mul(daiPot.chi()).add(daiVat.dai(address(this))) >= totalBalance.mul(DAI_ONE));
+        cash.transfer(msg.sender, _extraCash);
         return true;
     }
     
@@ -105,22 +110,24 @@ contract SigSimpleUniverse is ISimpleUniverse {
         DAILYBLOCKRATE = dailyblockrate;
     }
 
+    function start_emit() public {
+        uint DBR = DAILYBLOCKRATE;
+        // Emit a signal for delayed execution of this transaction
+        emitsig DailySignal().delay(DBR);
+    }
+    
     constructor(Augur _augur, uint dailyblockrate) public {
         DAILYBLOCKRATE = dailyblockrate;
         augur = _augur;
         cash = Cash(augur.lookup("Cash"));
-        daiVat = TestNetDaiVat(augur.lookup("DaiVat"));
-        daiPot = TestNetDaiPot(augur.lookup("DaiPot"));
-        daiJoin = TestNetDaiJoin(augur.lookup("DaiJoin"));
+        daiVat = IDaiVat(augur.lookup("DaiVat"));
+        daiPot = IDaiPot(augur.lookup("DaiPot"));
+        daiJoin = IDaiJoin(augur.lookup("DaiJoin"));
         daiVat.hope(address(daiPot));
         daiVat.hope(address(daiJoin));
         cash.approve(address(daiJoin), 2 ** 256 - 1);
         // bind slot to signal
         Update.bind(DailySignal);
-        
-        uint DBR = DAILYBLOCKRATE;
-        // Emit a signal for delayed execution of this transaction
-        emitsig DailySignal().delay(DBR);
     }
 
 }
